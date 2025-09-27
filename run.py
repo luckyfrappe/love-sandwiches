397ead7d286bd01acc7646cf84e60576a12eeffe
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-09-27 22:03:20 +0200

    Add use request loop

--- run.py
+++ run.py
@@ -20,15 +20,19 @@ def get_sales_data():
     via the terminal, which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
 
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+            
 
 def validate_data(values):
     """
@@ -44,9 +48,10 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
     else:
-        
         print(values)
+        return True
 
 
-get_sales_data()
+data = get_sales_data()
